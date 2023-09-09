365b0bed5d48d4621c062cdeffa363c7f5090a22
SunilVerma0358 <sunilverma93060@gmail.com>
2023-09-09 16:15:46 +0530

    modify done

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,12 @@
 const a = document.getElementById("nav")
+const bgimg = document.querySelector(".bg_img")
+const b = document.querySelector("#toggle")
 
 function responsive() {
     a.classList.toggle("show")
+    b.classList.toggle("overflow_hidden")
 }
+a.addEventListener("click", function() {
+    a.classList.toggle("show")
+    b.classList.toggle("overflow_hidden")
+})
