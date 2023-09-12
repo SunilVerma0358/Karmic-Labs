cb2207d06fb845193d734e46436dd585888d61a1
SunilVerma0358 <sunilverma93060@gmail.com>
2023-09-12 16:18:39 +0530

    arrow,extra-spaceing

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,16 @@
 const a = document.getElementById("nav")
 const bgimg = document.querySelector(".bg_img")
 const b = document.querySelector("#toggle")
+var line1 = document.querySelector(".line1")
+var line2 = document.querySelector(".line2")
+var line3 = document.querySelector(".line3")
 
 function responsive() {
     a.classList.toggle("show")
     b.classList.toggle("overflow_hidden")
+    line1.classList.toggle("fixed1")
+    line2.classList.toggle("fixed2")
+    line3.classList.toggle("fixed3")
 }
 a.addEventListener("click", function() {
     a.classList.toggle("show")
